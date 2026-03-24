651cf46fb4b7b96aebdb98ab454af9bef1e346f6
Talhah <talhah.ahmad1@ucalgary,ca>
2026-03-24 17:04:16 -0600

    Add (tentative) Experimental Setup

--- decision-tree-regression.py
+++ decision-tree-regression.py
@@ -9,25 +9,34 @@ df = pd.read_csv('Sales.csv')
 # Preprocessing plan
 # ------------------
 
+# Filter out non-bicycle sales
+df = df[df["Product_Category"] == "Bikes"]
+
 # Convert date strings to Timestamp object, and then sort DateFrame by date in ascending order
 df["Date"] = pd.to_datetime(df["Date"], format='%Y-%m-%d')
 df.sort_values(by=["Date"], inplace=True)
 
-# Create new DateFrame grouped by total unit sales in a month
-# df_monthly = df.groupby([df["Date"].dt.year, df["Date"].dt.month])["Order_Quantity"].sum()
+# Create new DataFrame grouped by total unit sales in a month in a location
+monthly_sales = df.groupby([df["Date"].dt.to_period('M'), "State"])["Order_Quantity"].sum().reset_index()
+monthly_sales.columns = ["Date", "State", "Order_Quantity"]
+
+# Add features to the new DataFrame to use as predictors
+monthly_sales["Year"] = monthly_sales["Date"].dt.year
+monthly_sales["Month"] = monthly_sales["Date"].dt.month
 
 # Initialize features matrix X and labels vector y
-X = df.drop(columns=["Order_Quantity"], axis=1)
-y = df["Order_Quantity"]
+X = monthly_sales[["Year", "Month", "State"]]
+y = monthly_sales["Order_Quantity"]
 
 # One-hot encode categorical columns
+X = pd.get_dummies(X)
 
 # Split chronologically with a roughly 70% train, 30% test split
-split = int(len(df) * 0.7)
-X_train = X[:split]
-X_test = X[split:]
-y_train = y[:split]
-y_test = y[split:]
+split = int(len(monthly_sales) * 0.7)
+X_train = X.iloc[:split]
+X_test = X.iloc[split:]
+y_train = y.iloc[:split]
+y_test = y.iloc[split:]
 
 # Standardize the input features
 scaler = StandardScaler()
