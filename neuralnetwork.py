e437854ccd2b1a495355993283e3e60bee74ff2e
KevinPham <kevinpp2007@gmail.com>
2026-03-30 11:43:48 -0600

    Update neuralnetwork.py

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,7 +1,7 @@
 from preprocessing import load_data
 from preprocessing import monthly_sales
 
-import matplotlib.pyplot as plt
+import matplotlib as plt
 import pandas as pd
 
 import tensorflow as tf
@@ -9,37 +9,38 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, InputLayer
 
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-tf.random.set_seed(42) #seed for reproducable results
+tf.random.set_seed(42)  # seed for reproducable results
 
-#load and preprocess dataset
+# load and preprocess dataset
 X_train, X_test, y_train, y_test = load_data()
 
-#Convert to float32 for TensorFlow (Chatgpt helped with this code to fix an error I was getting about data types)
+# Convert to float32 for TensorFlow
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
 y_train = y_train.astype('float32')
 y_test = y_test.astype('float32')
 
-#Build Neural Network Model
+# Build Neural Network Model
 neural_network_model = Sequential()
 
-#Input layer
+# Input layer
 neural_network_model.add(InputLayer(input_shape=(X_train.shape[1],)))
 
-#Hidden layers
-neural_network_model.add(Dense(64, activation='relu')) #density of 64
-neural_network_model.add(Dense(32, activation='relu')) #density of 32
+# Hidden layers
+neural_network_model.add(Dense(128, activation='relu'))
+neural_network_model.add(Dense(64, activation='relu'))  # density of 64
+neural_network_model.add(Dense(32, activation='relu'))  # density of 32
 
-#Output layer (1 neuron for regression)
+# Output layer (1 neuron for regression)
 neural_network_model.add(Dense(1))
 
-#Compile model
+# Compile model
 neural_network_model.compile(optimizer='adam', loss='mse')
 
-#train model
-history = neural_network_model.fit(X_train, y_train, epochs=50, validation_split=0.2)
+# Train model
+history = neural_network_model.fit(X_train, y_train, epochs=100, validation_split=0.2)
 
-#Evaluate
+# Evaluate
 y_pred = neural_network_model.predict(X_test).flatten()
 
 MAE = mean_absolute_error(y_test, y_pred)
@@ -53,7 +54,7 @@ print(f"R2: {R2}")
 # get
 df = pd.read_csv('Sales.csv')
 
-#Put test dates, actual values, and predicted values together
+# Put test dates, actual values, and predicted values together
 months = monthly_sales["Date"].drop_duplicates().sort_values()
 split = int(len(months) * 0.7)
 test_months = months.iloc[split:]
