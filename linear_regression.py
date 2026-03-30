7d73b20f6212361c0087ac34aa07c07c37623c1f
Matthew Yan <0519matthew@gmail.com>
2026-03-30 01:50:12 -0600

    Prepare data for plotting

--- linear_regression.py
+++ linear_regression.py
@@ -1,4 +1,7 @@
+import pandas as pd
+
 from preprocessing import load_data
+from preprocessing import monthly_sales
 
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.linear_model import LinearRegression
@@ -24,3 +27,11 @@ print('MAE:', MAE)
 print('RMSE:', RMSE)
 print('R2:', R2)
 
+# Gets unique months from dataset.
+months = monthly_sales["Date"].drop_duplicates().sort_values()
+# Train-test split (70/30).
+split = int(len(months) * 0.7)
+# Selects months used for testing.
+test_months = months.iloc[:split]
+# Checks if Date from each row in monthly_sales is in test_months.
+months_test = monthly_sales["Date"].isin(test_months)
