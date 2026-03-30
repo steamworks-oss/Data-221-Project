a6cc3348a4e5c45291f4115dd153634ca8b29a80
Matthew Yan <0519matthew@gmail.com>
2026-03-30 00:10:18 -0600

    imported required packages for model

---
+++ linear_regression.py
@@ -0,0 +1,6 @@
+from preprocessing import load_data
+
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
