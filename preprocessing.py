bb8069e39a6a399cfad55b1098c68d004dd4fedf
Talhah <talhah.ahmad1@ucalgary,ca>
2026-03-29 23:30:36 -0600

    Updated to be able to import dataset

--- preprocessing.py
+++ preprocessing.py
@@ -1,30 +1,30 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 
+# load dataset
+df = pd.read_csv('Sales.csv')
+
+# ------------------
+# Preprocessing plan
+# ------------------
+
+# Filter out non-bicycle sales
+df = df[df["Product_Category"] == "Bikes"]
+
+# Convert date strings to Timestamp object, and then sort DateFrame by date in ascending order
+df["Date"] = pd.to_datetime(df["Date"], format='%Y-%m-%d')
+df.sort_values(by=["Date"], inplace=True)
+
+# Create new DataFrame grouped by total unit sales in a month in a location
+monthly_sales = df.groupby([df["Date"].dt.to_period('M'), "State"])["Order_Quantity"].sum().reset_index()
+monthly_sales.columns = ["Date", "State", "Order_Quantity"]
+monthly_sales["Date"] = monthly_sales["Date"].dt.to_timestamp()
+
+# Add features to the new DataFrame to use as predictors
+monthly_sales["Year"] = monthly_sales["Date"].dt.year
+monthly_sales["Month"] = monthly_sales["Date"].dt.month
+
+# Function that returns the train/test data
 def load_data():
-    # load dataset
-    df = pd.read_csv('Sales.csv')
-
-    # ------------------
-    # Preprocessing plan
-    # ------------------
-
-    # Filter out non-bicycle sales
-    df = df[df["Product_Category"] == "Bikes"]
-
-    # Convert date strings to Timestamp object, and then sort DateFrame by date in ascending order
-    df["Date"] = pd.to_datetime(df["Date"], format='%Y-%m-%d')
-    df.sort_values(by=["Date"], inplace=True)
-
-    # Create new DataFrame grouped by total unit sales in a month in a location
-    monthly_sales = df.groupby([df["Date"].dt.to_period('M'), "State"])["Order_Quantity"].sum().reset_index()
-    monthly_sales.columns = ["Date", "State", "Order_Quantity"]
-    monthly_sales["Date"] = monthly_sales["Date"].dt.to_timestamp()
-
-    # Add features to the new DataFrame to use as predictors
-    monthly_sales["Year"] = monthly_sales["Date"].dt.year
-    monthly_sales["Month"] = monthly_sales["Date"].dt.month
-
     # Initialize features matrix X and labels vector y
     X = monthly_sales[["Year", "Month", "State"]]
     y = monthly_sales["Order_Quantity"]
