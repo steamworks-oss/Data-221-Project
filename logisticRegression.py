115b9e5ece49cdb1e857e3df45d5857ad314ea01
TiffanyTran-184 <tiffany.tran1@ucalgary.ca>
2026-03-30 13:16:38 -0600

    update logistic regression and delete __pycache__

--- logisticRegression.py
+++ logisticRegression.py
@@ -1,10 +1,14 @@
 from sklearn.linear_model import LogisticRegression
-from preprocessing import load_and_preprocess
+from preprocessing import load_data
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
+
 
 # 1. Load and Split Data
-X_train, X_test, y_train, y_test, monthly_sales = load_and_preprocess()
+X_train, X_test, y_train, y_test, monthly_sales, train_dates, test_dates = load_data()
 
 
 # 2. Initialize and Train
@@ -41,12 +45,9 @@ y = monthly_sales["Sales_Level"]
 labelEncode = LabelEncoder()
 targetVariable = labelEncode.fit_transform(monthly_sales["Sales_Level"])
 
-# X_train, X_test already imported from preprocessing
-split = len(X_train)  # same index used in preprocessing
-
-# Split the new categorical target
-y_train_category = targetVariable[:split]  # matches X_train
-y_test_category = targetVariable[split:]   # matches X_test
+# correct split using train_dates and test_dates
+y_train_category = targetVariable[train_dates]
+y_test_category = targetVariable[test_dates]
 # Train model
 classifier = LogisticRegression(max_iter=1000)
 classifier.fit(X_train, y_train_category)
@@ -56,9 +57,35 @@ accuracy_score = classifier.score(X_test, y_test_category)
 print(f"Model Accuracy: {accuracy_score:.2f}")
 
 # The accuracy improve from 0.08 to 0.69
-from sklearn.metrics import confusion_matrix
-y_pred = classifier.predict(X_test)
-conf_matrix = confusion_matrix(y_test_category, y_pred)
-print(conf_matrix)
-from sklearn.metrics import classification_report
-print(classification_report(y_test_category, y_pred, target_names=["Low","Medium","High"]))
+y_prediction = classifier.predict(X_test)
+
+conf_matrix = confusion_matrix(y_test_category, y_prediction)
+print("confustion matrix",conf_matrix)
+print(classification_report(y_test_category, y_prediction, target_names=["Low","Medium","High"]))
+
+# plot actual and predict values over time.
+# Display results
+# ------------------
+mapping = {"Low": 0, "Medium": 1, "High": 2}
+results = pd.DataFrame({
+    "Date": monthly_sales.loc[test_dates, "Date"],
+    "Actual": labelEncode.inverse_transform(y_test_category),
+    "Predicted": labelEncode.inverse_transform(y_prediction)
+})
+
+results["Actual_num"] = results["Actual"].map(mapping)
+results["Predicted_num"] = results["Predicted"].map(mapping)
+
+plot_values = results.groupby("Date")[["Actual_num", "Predicted_num"]].mean().reset_index()
+
+plt.plot(plot_values["Date"], plot_values["Actual_num"], label="Actual")
+plt.plot(plot_values["Date"], plot_values["Predicted_num"], label="Predicted")
+
+plt.xlabel("Date")
+plt.ylabel("Sales Level (0=Low,1=Med,2=High)")
+plt.title("Actual vs Predicted Sales Level")
+plt.xticks(rotation=45)
+plt.legend()
+plt.tight_layout()
+plt.show()
+
