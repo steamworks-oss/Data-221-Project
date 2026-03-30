49c057693154ca35331c547ee704d36046536b66
KevinPham <kevinpp2007@gmail.com>
2026-03-30 00:33:44 -0600

    Refactor neural network model and evaluation metrics
    Updated the neural network model architecture and improved the evaluation metrics. Adjusted hidden layer sizes and added a random seed for reproducibility.

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,6 +1,7 @@
 from preprocessing import load_data
 from preprocessing import monthly_sales
 
+import tensorflow as tf
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import pandas as pd
@@ -8,6 +9,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, InputLayer
 
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+tf.random.set_seed(42)
 
 #load and preprocess dataset
 X_train, X_test, y_train, y_test = load_data()
@@ -25,7 +27,7 @@ neural_network_model = Sequential()
 neural_network_model.add(InputLayer(input_shape=(X_train.shape[1],)))
 
 #Hidden layers
-neural_network_model.add(Dense(32, activation='relu'))
+neural_network_model.add(Dense(64, activation='relu'))
 neural_network_model.add(Dense(32, activation='relu'))
 
 #Output layer (1 neuron for regression)
